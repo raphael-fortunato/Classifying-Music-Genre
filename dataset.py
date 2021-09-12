ddf405e85d2bf871f9480435681c45be8bff6927
4170512 <r.furtunato@students.uu.nl>
2021-09-12 16:59:00 +0200

    refactored code and commented

--- dataset.py
+++ dataset.py
@@ -1,14 +1,17 @@
 import os
+import numpy as np
+import torch
+import torchaudio
 from torch.utils.data import Dataset
 
 class MusicDataset(Dataset):
-
     def __init__(self, root):
         super().__init__()
         self.root = root
         self.data = []
         self.labels = []
         self.classes = []
+        # self.transform = transform
         for path, dir_names, files in os.walk(root):
             if not self.classes:
                 self.classes = dir_names
@@ -27,3 +30,48 @@ class MusicDataset(Dataset):
         audio = torchaudio.load(fname)[0]
         class_idx = self.labels[i]
         return audio, class_idx
+
+# padding audio files to ensure equal length
+def pad_sequence(batch):
+    # Make all tensor in a batch the same length by padding with zeros
+    batch = [item.t() for item in batch]
+    batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True, padding_value=0.)
+    return batch.permute(0, 2, 1)
+
+# used to create test/train batches
+def collate_fn(batch):
+    # A data tuple has the form:
+    # waveform, sample_rate, label, speaker_id, utterance_number
+    tensors, targets = [], []
+    # Gather in lists, and encode labels as indices
+    for waveform, label in batch:
+        tensors += [waveform]
+        targets += [torch.tensor(label)]
+    # Group the list of tensors into a batched tensor
+    tensors = pad_sequence(tensors)
+    targets = torch.stack(targets)
+    return tensors, targets
+
+# create dataloaders to train CNN
+def get_dataset(args):
+    train_dataset = MusicDataset(args.root)
+    valid_dataset = MusicDataset(args.root)
+
+    len_dataset = len(train_dataset)
+    indices = list(range(len_dataset))
+    split = int(np.floor(.9 * len_dataset))
+    np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
+    valid_sampler = torch.utils.data.SubsetRandomSampler(valid_idx)
+
+    # load dataset
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, sampler=train_sampler,
+        collate_fn=collate_fn, num_workers=args.num_workers)
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=args.batch_size, sampler=valid_sampler,
+        collate_fn=collate_fn, num_workers=args.num_workers)
+
+    return {'train': train_loader, 'valid':valid_loader}
--- main.py
+++ main.py
@@ -4,44 +4,26 @@ import torch
 from torchaudio.datasets import GTZAN
 from torchaudio.datasets.utils import download_url
 
-from dataset import MusicDataset
+from dataset import MusicDataset, get_dataset
 from args import get_args
 
-
-def get_dataset(args):
-    train_dataset = MusicDataset(args.root)
-    valid_dataset = MusicDataset(args.root)
-    num_train = .9
-    len_dataset = len(train_dataset)
-    indices = list(range(len_dataset))
-    split = int(np.floor(.9 * num_train))
-
-    np.random.shuffle(indices)
-
-    train_idx, valid_idx = indices[split:], indices[:split]
-    train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
-    valid_sampler = torch.utils.data.SubsetRandomSampler(valid_idx)
-
-    # load dataset
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, sampler=train_sampler,
-        num_workers=args.num_workers)
-    valid_loader = torch.utils.data.DataLoader(
-        valid_dataset, batch_size=args.batch_size, sampler=valid_sampler,
-        num_workers=args.num_workers)
-
-    return {'train': train_loader, 'valid':valid_loader}
-
 if __name__ == '__main__':
+    # get and print arguments
     args = get_args()
     for arg in vars(args):
         print(arg, getattr(args, arg))
+    # set seeds
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    datasets = get_dataset(args)
-
-
+    # read in data and dataloaders
+    dataloaders = get_dataset(args)
+    # set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    for audio, label in dataloaders['train']:
+        print(label)
+        print(audio)
+
     print("Using device: ", device)
 
 
